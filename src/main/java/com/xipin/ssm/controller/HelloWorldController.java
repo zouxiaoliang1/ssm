994e0266b0400119aa371aca717c66ba0c9bab00
zouxiaoliang <zxl@xipintech.com>
2016-06-23 16:51:46 +0800

    练习freemarker

--- src/main/java/com/xipin/ssm/controller/HelloWorldController.java
+++ src/main/java/com/xipin/ssm/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.xipin.ssm.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,14 +16,17 @@ public class HelloWorldController {
 	@RequestMapping("/helloWorld")
 	public String helloWorld(@ModelAttribute("user") User user, Model model) {
 		System.out.println(user.getUserName() + "-----" + user.getPassword());
-		model.addAttribute("message", "hello world");
+		String[] str = new String[]{"you", "me", "he"};
+		model.addAttribute("message", str);
 		return "index";
 	}
 	
-	@RequestMapping("/freemaker")
-	public String freemaker(@ModelAttribute("user") User user, Model model) {
+	@RequestMapping("/freemarker")
+	public Map<String, Object> freemarker(@ModelAttribute("user") User user, Model model) {
 		System.out.println(user.getUserName() + "-----" + user.getPassword());
 		model.addAttribute("message", "hello world");
-		return "index";
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("a", 1);
+		return map;
 	}
 }
--- src/main/webapp/WEB-INF/ftl/index.ftl
+++ src/main/webapp/WEB-INF/ftl/index.ftl
@@ -1,6 +1,6 @@
 <html>
 <head>
-<meta charset="UTF-8">
+<#setting url_escaping_charset='utf-8'>
 <title>Insert title here</title>
 </head>
 <body>
@@ -9,7 +9,19 @@
 		<input type="text" name="password">
 		<button>提交</button>
 	</form>
-	<h3>用户名：${user.userName}</h3>
-	<h3>密码：${user.password}</h3>
+	<h3>用户名：${user.userName!}</h3>
+	<h3>密码：${user.password!}</h3>
+	<br>
+	<#if (2>3)>
+                          二比三大
+    <#else>
+                          三比二大
+    </#if>
+    <br>
+    <#list ["dd", "sd"] as l>
+    	${l}
+    </#list>
+    <br>
+    ${message[0].userName!}
 </body>
 </html>
